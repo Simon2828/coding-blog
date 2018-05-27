6cad1cc787f1f70a467e17a89137f59f96f2181c
simon2828 <simon.tanner28@gmail.com>
2018-05-27 18:06:21 +0100

    update about

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,14 +16,11 @@ const Navbar = () => (
         <Link className="navbar-item" to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
       </div>
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/Simon2828"
           target="_blank"
           rel="noopener noreferrer"
         >
